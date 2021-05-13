4e2375fde42a8f38f7063a96b2cca071674931df
Andrey Tabakov <komdosh@yandex.ru>
2021-05-13 13:24:51 +0300

    feat(go): fix print, get, add methods

--- 2-linked-lists/src/linked-list.go
+++ 2-linked-lists/src/linked-list.go
@@ -33,11 +33,10 @@ func (l *LinkedList) Get(position int64) (*Node, error) {
 
 	var current = l.start
 	for current != nil {
-		position = -1
 		if position == 0 {
 			return current, nil
 		}
-
+		position--
 		current = current.next
 	}
 
@@ -56,7 +55,8 @@ func (l *LinkedList) Add(item int32) error {
 		l.size = 1
 	} else {
 		l.end.next = node
-		l.size = l.size + 1
+		l.end = node
+		l.size++
 	}
 	return nil
 }
@@ -64,13 +64,29 @@ func (l *LinkedList) Add(item int32) error {
 func (l *LinkedList) Remove(node *Node) error {
 	var current = l.start
 	for current != nil {
+
 		if current == node {
-			if node == l.start {
-				l.start = current
+			if current == l.start {
+				l.start = current.next
+			} else if current.next != nil {
+				current.next = current.next.next
+			} else {
+				current.next = nil
 			}
-			current.next = node.next
+			l.size--
 			break
 		}
+
+		current = current.next
+
+	}
+	return nil
+}
+
+func (l *LinkedList) Print() error {
+	var node = l.start
+	for node != nil {
+		fmt.Printf("%d\n", node.item)
 
 		node = node.next
 	}
@@ -83,33 +99,25 @@ func main() {
 	l.Add(1)
 	l.Add(2)
 
-	var node = l.start
-	for node != nil {
-		fmt.Printf("%d\n", node.item)
+	l.Print()
 
-		node = node.next
+	if node, err := l.Get(2); err == nil {
+		l.Remove(node)
+	} else {
+		fmt.Printf("%s\n", err.Error())
+	}
+
+	if node, err := l.Get(1); err == nil {
+		l.Remove(node)
+	} else {
+		fmt.Printf("%s\n", err.Error())
 	}
 
 	if node, err := l.Get(0); err == nil {
 		l.Remove(node)
 	} else {
-		fmt.Printf("%s", err.Error())
-	}
-	if node, err := l.Get(0); err == nil {
-		l.Remove(node)
-	} else {
-		fmt.Printf("%s", err.Error())
-	}
-	if node, err := l.Get(0); err == nil {
-		l.Remove(node)
-	} else {
-		fmt.Printf("%s", err.Error())
+		fmt.Printf("%s\n", err.Error())
 	}
 
-	node = l.start
-	for node != nil {
-		fmt.Printf("%d\n", node.item)
-
-		node = node.next
-	}
+	l.Print()
 }
