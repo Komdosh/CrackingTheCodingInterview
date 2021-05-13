e553c96de514f258af82e0c677e232add535d228
Andrey Tabakov <komdosh@yandex.ru>
2021-05-13 09:49:51 +0300

    feat(go): create linked list impl for tasks

---
+++ 2-linked-lists/src/linked-list.go
@@ -0,0 +1,115 @@
+package main
+
+import "fmt"
+
+type LinkedList struct {
+	start *Node
+	end   *Node
+	size  int64
+}
+
+type Node struct {
+	item int32
+	next *Node
+}
+
+func createLinkedList() LinkedList {
+	return LinkedList{
+		start: nil,
+		end:   nil,
+		size:  0,
+	}
+}
+
+func (l *LinkedList) GetSize() int64 {
+	return l.size
+}
+
+func (l *LinkedList) Get(position int64) (*Node, error) {
+
+	if position >= l.GetSize() {
+		return nil, fmt.Errorf("position greater than size of list")
+	}
+
+	var current = l.start
+	for current != nil {
+		position = -1
+		if position == 0 {
+			return current, nil
+		}
+
+		current = current.next
+	}
+
+	return nil, fmt.Errorf("shouldn't be called")
+}
+
+func (l *LinkedList) Add(item int32) error {
+	node := &Node{
+		item,
+		nil,
+	}
+
+	if l.end == nil {
+		l.end = node
+		l.start = node
+		l.size = 1
+	} else {
+		l.end.next = node
+		l.size = l.size + 1
+	}
+	return nil
+}
+
+func (l *LinkedList) Remove(node *Node) error {
+	var current = l.start
+	for current != nil {
+		if current == node {
+			if node == l.start {
+				l.start = current
+			}
+			current.next = node.next
+			break
+		}
+
+		node = node.next
+	}
+	return nil
+}
+
+func main() {
+	l := createLinkedList()
+	l.Add(0)
+	l.Add(1)
+	l.Add(2)
+
+	var node = l.start
+	for node != nil {
+		fmt.Printf("%d\n", node.item)
+
+		node = node.next
+	}
+
+	if node, err := l.Get(0); err == nil {
+		l.Remove(node)
+	} else {
+		fmt.Printf("%s", err.Error())
+	}
+	if node, err := l.Get(0); err == nil {
+		l.Remove(node)
+	} else {
+		fmt.Printf("%s", err.Error())
+	}
+	if node, err := l.Get(0); err == nil {
+		l.Remove(node)
+	} else {
+		fmt.Printf("%s", err.Error())
+	}
+
+	node = l.start
+	for node != nil {
+		fmt.Printf("%d\n", node.item)
+
+		node = node.next
+	}
+}
