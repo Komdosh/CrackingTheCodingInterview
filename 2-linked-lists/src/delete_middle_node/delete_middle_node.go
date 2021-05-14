ebbd40874391f49c71bbd9d44878d45c1c3d48a6
Andrey Tabakov <komdosh@yandex.ru>
2021-05-14 10:40:54 +0300

    feat(task2.3): delete middle node

---
+++ 2-linked-lists/src/delete_middle_node/delete_middle_node.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"../../src/list"
+	"fmt"
+)
+
+// Delete Middle Node: Implement an algorithm to delete a node in the middle
+// (i.e., any node but the first and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
+
+func main() {
+	l := list.CreateLinkedList()
+
+	l.Add(0)
+	l.Add(1)
+	l.Add(2)
+	l.Add(3)
+	l.Add(4)
+	l.Add(5)
+
+	l.Print()
+
+	if n, e := l.Get(2); e == nil {
+		DeleteMiddle(n)
+
+		l.Print()
+	} else {
+		fmt.Printf("Fetch by position error")
+	}
+}
+
+// DeleteMiddle delete node in singly linked list by pointer to its node. We don't have start pointer.
+func DeleteMiddle(n *list.Node) error {
+	if n == nil || n.Next == nil {
+		return fmt.Errorf("can't delete nil or last element")
+	}
+
+	n.Item = n.Next.Item
+	n.Next = n.Next.Next
+	return nil
+}
